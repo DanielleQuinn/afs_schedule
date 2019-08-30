7b9ea4a106a8d576f5f68d24c6953ec10b4d66e6
Danielle Quinn <danielle.quinn@acadiau.ca>
2019-08-30 17:47:07 -0230

    Update process_links.R

--- process_links.R
+++ process_links.R
@@ -1,6 +1,11 @@
+# Load libraries
+library(dplyr)
+library(rvest)
+library(stringr)
+
 # This reads the links.txt file as an html and extracts every link from it as a vector
 mylinks <- read_html(links.txt) %>% 
   html_nodes("a") %>% 
   html_attr("href") %>% 
   unique() %>%
-  stringr::str_subset("session")
+  str_subset("session")

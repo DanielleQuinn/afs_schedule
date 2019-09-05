7711052c31102a7dd47aab972aa95e77f7469e31
Danielle Quinn <danielle.quinn@acadiau.ca>
2019-09-05 16:19:26 -0230

    Update process_links.R

--- process_links.R
+++ process_links.R
@@ -1,11 +1,15 @@
-# Load libraries
-library(dplyr)
-library(rvest)
-library(stringr)
+# To use extract_schedule() I need to be able to pull
+# all of the links from https://afstws2019.org/detailed-schedule/
+# Apparently, javascript is doing something to modify the
+# page after loading so it was tricky to automate ...
+# I finally resorted to copy/pasting the raw html code
+# directly from the website and saving it -_-
+# This file can be found at https://github.com/DanielleQuinn/afs_schedule/blob/master/links.txt
 
-# This reads the links.txt file as an html and extracts every link from it as a vector
-mylinks <- read_html(links.txt) %>% 
+# Using this file, extract all of the links to each session
+# and tidy for use with extract_schedule()
+mylinks <- read_html("C:/Users/Danielle/Desktop/testme.txt") %>% 
   html_nodes("a") %>% 
   html_attr("href") %>% 
   unique() %>%
-  str_subset("session")
+  stringr::str_subset("session")

7541a22c7d10c7cab5c99e3078c5808d5aed9e13
Danielle Quinn <danielle.quinn@acadiau.ca>
2019-09-05 16:18:42 -0230

    Update CatchupPause.R

--- functions/CatchupPause.R
+++ functions/CatchupPause.R
@@ -1,9 +1,12 @@
-# Sourced from https://stackoverflow.com/users/5778374/nm200
-# https://stackoverflow.com/questions/37839566/how-do-i-close-unused-connections-after-read-html-in-r
-
-# Solution for connections timing out when looping through all links
+# With hundreds of links to loop through, it seemed to
+# time out. I found this solution / function on
+# StackOverflow and it fixed the problem
+# post: https://stackoverflow.com/questions/37839566/how-do-i-close-unused-connections-after-read-html-in-r
+# user: https://stackoverflow.com/users/5778374/nm200
 CatchupPause <- function(Secs){
   Sys.sleep(Secs) #pause to let connection work
   closeAllConnections()
   gc()
 }
+
+
